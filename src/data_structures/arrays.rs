fe9cef10ccb0e1e16d7dde2ace3a3643057d7b0a
Am0rfu5 <am0fu5@protonmail.me>
2024-01-17 21:26:30 -0600

    modifications to arrays

--- src/data_structures/arrays.rs
+++ src/data_structures/arrays.rs
@@ -4,7 +4,7 @@
  * Accessing an element at a specific index in an array is a
  * constant time operation.
  */
-pub fn access_element(array: &[i32], index: usize) -> Option<i32> {
+pub fn access_array_element(array: &[i32], index: usize) -> Option<i32> {
     array.get(index).copied()
 }
 
@@ -13,9 +13,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_access_element() {
+    fn test_access_array_element() {
         let array = [1, 2, 3, 4, 5];
-        assert_eq!(access_element(&array, 0), Some(1));
-        assert_eq!(access_element(&array, 6), None);
+        assert_eq!(access_array_element(&array, 0), Some(1));
+        assert_eq!(access_array_element(&array, 6), None);
     }
 }
